fadc5354e1f4913e2c5959681661ee7797c9692d
kostas-111 <galkink370@gmail.com>
2024-01-25 23:55:08 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surname равен: " + surname.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Vladislav Galkin";
+        names[1] = "Dmitriy Korneev";
+        names[2] = "Daria Dushina";
+        names[3] = "John Deere";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
