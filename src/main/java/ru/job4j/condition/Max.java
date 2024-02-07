d7f4b2a71139d423dd627e78afaed6f564e5f179
kostas-111 <galkink370@gmail.com>
2024-02-07 22:49:32 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,14 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left >= right ? left : right;
-        return result;
+        return left >= right ? left : right;
+    }
+
+    public static int max(int a, int b, int c) {
+        return max(max(a, b), c);
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return max(max(a, b, c), d);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,99 @@ class MaxTest {
         int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax3Or1Or4Then4() {
+        int a = 3;
+        int b = 1;
+        int c = 4;
+        int result = Max.max(a, b, c);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3Or0OrMinus4Then3() {
+        int a = 3;
+        int b = 0;
+        int c = -4;
+        int result = Max.max(a, b, c);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMaxMinus3Or0OrMinus4Then3() {
+        int a = -3;
+        int b = 0;
+        int c = -4;
+        int result = Max.max(a, b, c);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3Or3Or3Then3() {
+        int a = 3;
+        int b = 3;
+        int c = 3;
+        int result = Max.max(a, b, c);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3Or1Or4Or5Then5() {
+        int a = 3;
+        int b = 1;
+        int c = 4;
+        int d = 5;
+        int result = Max.max(a, b, c, d);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3Or1Or6Or5Then6() {
+        int a = 3;
+        int b = 1;
+        int c = 6;
+        int d = 5;
+        int result = Max.max(a, b, c, d);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax3Or7Or6OrMinus5Then6() {
+        int a = 3;
+        int b = 7;
+        int c = 6;
+        int d = -5;
+        int result = Max.max(a, b, c, d);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax8Or1Or0Or0Then6() {
+        int a = 8;
+        int b = 1;
+        int c = 0;
+        int d = 3;
+        int result = Max.max(a, b, c, d);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1Or1Or1Or1Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int d = 1;
+        int result = Max.max(a, b, c, d);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
 }
